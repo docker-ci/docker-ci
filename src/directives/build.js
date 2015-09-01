fca2c748703ccd812f1436103ad2407444f63c2e
Ivan.Orlov <ivan@morrr.com>
2015-09-01 10:34:07 +0300

    Custom file for build directive

--- src/directives/build.js
+++ src/directives/build.js
@@ -1,25 +1,25 @@
 var _ = require('lodash');
 var domain = require('wires-domain');
-var Promise = require('promise')
-
+var Promise = require('promise');
 
 domain.service("directive.build", function(Bash) {
    return function(dir) {
-      var tag = dir.requireOne("tag").getValue()
+      var tag = dir.requireOne("tag").getValue();
       var path = dir.getFirst("path", ".").getValue();
+      var cache = dir.getFirst("cache", "true").getValue() === "true";
 
-      var cache = dir.getFirst("cache", "true").getValue() === "true"
-
+      var dockerFile = dir.getFirst("file", "Dockerfile").getValue();
       var buider = new Bash("docker");
-      buider.add("build")
+
+      buider.add("build");
+      buider.add("--file=" + dockerFile);
+
       if (!cache) {
-         buider.add("--no-cache=true")
+         buider.add("--no-cache=true");
       }
-
       buider.add("-t", tag, path);
-
       return buider.call({
          printOutput: true
       });
-   }
-})
+   };
+});
