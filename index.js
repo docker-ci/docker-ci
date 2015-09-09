e99294c7100670e9130c35155148080ca9e49ab1
Ivan.Orlov <ivan@morrr.com>
2015-09-09 09:51:21 +0300

    adding datetime argument

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 
 var _ = require('lodash');
 var domain = require('wires-domain');
-var server = require('wires-client')
-var Promise = require('promise')
+var server = require('wires-client');
+var Promise = require('promise');
 var path = require("path");
+var momemnt = require("moment");
 require('require-all')(__dirname + '/src');
 
 domain.require(function(DockerCi, Bash, $scope) {
    var rootPath = path.join("/", process.cwd());
-   $scope.setArg("dir", rootPath)
+   $scope.setArg("dir", rootPath);
+   $scope.setArg("datetime", moment().format('DD-MMMM-YYYY-hh_mm'));
    DockerCi.init();
-})
+});
